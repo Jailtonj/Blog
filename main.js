5fc40c49f7f460722d208f16350fc28c508306c4
jailtonj <jailtonpereira762@gmail.com>
2022-11-29 20:02:32 -0300

    Add Search Bar

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const google = 'https://www.google.com/search?q=site%3A+';
+const site = 'pagedart.com';
+
+function submitted(event) {
+  event.preventDefault();
+  const url = google + site + '+' + q.value;
+  const win = window.open(url, '_blank');
+  win.focus();
+}
+f.addEventListener('submit', submitted);
+
+
 let delay = -0.4;
 function createCard(title, date, content) {
     delay = delay + 0.4;
