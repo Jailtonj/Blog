30d3703a9fa747d758cb281a1fd6c74dd8498e3a
jailtonj <jailtonpereira762@gmail.com>
2022-11-29 19:15:36 -0300

    adc conteúdo

--- main.js
+++ main.js
@@ -20,26 +20,45 @@ function createCard(title, date, content) {
   }
 
   document.querySelector("#cards").innerHTML = 
-      createCard("Tim Berners-Lee vai leiloar código-fonte da web",
+      createCard("Reino Unido vai multar empresas de tecnologia que não removerem conteúdo de automutilação",
 
-      "02 de jul, 2021", 
+      "27/11/2022", 
       
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero." )
+      "O governo do Reino Unido pretende tornar ilegal o ato de encorajar outras pessoas a se mutilar na internet e multará as empresas de mídia social que não removerem esse tipo material, como parte de uma reformulação da lei que rege o comportamento online." )
+      
+      +
+
+      createCard("Estádios brasileiros buscam tecnologias parecidas com os do Qatar",
+
+      "29/11/2022", 
+      
+      "Com alto investimento em tecnologia, a Copa do Mundo no Qatar promete contar com um dos maiores investimentos da história neste setor. Para entrar no país, os torcedores precisam ter o Haya Card, uma espécie de ''passaporte'' que servirá como um documento para os turistas. Além disso, o cartão será utilizado para o controle na venda de bebidas alcoólicas, transporte público e entrada nos estádios." )
 
       +
 
-      createCard("Tem Firefox novo no pedaço e você vai quer migrar",
+      createCard("WhatsApp lança mensagens para o próprio número no Android e iPhone",
 
-      "02 de jul, 2021",
+      "28/11/2022",
 
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est.")
+      "A partir desta segunda-feira (28), usuários do WhatsApp no Android e iOS (iPhone) poderão iniciar uma conversa com o próprio contato. A novidade, que já vinha sendo liberada para algumas pessoas, elimina a gambiarra de criar um grupo e depois manter o próprio número.")
 
        +
 
-      createCard("John MCAfee, criador do antivírus McAfee, morre",
+      createCard("Live de Casimiro bate novo recorde de espectadores em jogo do Brasil",
 
-      "02 de jul, 2021",
-      
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit lectus libero, id efficitur arcu viverra ac. Etiam sed sagittis velit, vitae convallis turpis. Praesent in purus non tellus tincidunt fermentum eu at sem. Curabitur lacus libero.")
-    
+      "28/11/2022",
+
+      "Na tarde desta segunda-feira (28), durante a transmissão do jogo entre Brasil e Suíça pela Copa do Mundo 2022, o streamer Casimiro Miguel bateu o seu próprio recorde de espectadores. De acordo com dados do próprio YouTube, a live atingiu mais de 4,3 milhões de dispositivos conectados.") +
+
+      createCard("Nubank lança seguro contra transferências após roubo de celular",
+
+      "28/11/2022",
+
+      "O Nubank anunciou, nesta segunda-feira (28), o lançamento de um novo seguro contra fraudes em transações feitas pelo aplicativo do banco. Caso o usuário tenha o celular furtado ou roubado, ou seja coagido a realizar alguma transação para outra conta, será possível recuperar o dinheiro.",) +
+
+      createCard("Correios lançam rastreamento em tempo real para encomendas Sedex",
+
+      "28/11/2022",
+
+      "Na última sexta-feira (25), durante a Black Friday, os Correios anunciaram o lançamento de uma nova opção no sistema de rastreamento de encomendas da empresa. A partir de agora, será possível realizar rastreamento em tempo real em pedidos feitos por Sedex convencional, Sedex 10, Sedex 12 e Sedex Hoje.",) 
 
